ddd1c35465300d5092a1963be4b8b7bfd891094a
adenmoy <adenmoy@canariaseducacion.es>
2026-02-26 19:04:27 +0000

    Commit de Sergio

--- src/main/java/com/mycompany/ut05actividadgitide/Prueba.java
+++ src/main/java/com/mycompany/ut05actividadgitide/Prueba.java
@@ -2,7 +2,7 @@ package com.mycompany.ut05actividadgitide;
 
 public class Prueba extends ImpNomCant {
     public static void main(String[] args) {
-        printCuenta("Mónica", 2);
+        printCuenta("Sergio", 2);
     }
 
 }
